380924d290d5d43f04eb3a666e708182942f9550
js-talentelgia <122963266+js-talentelgia@users.noreply.github.com>
2025-08-21 22:18:23 +0530

    update .sh files

--- start_container.sh
+++ start_container.sh
@@ -4,7 +4,7 @@ set -e
 sudo su - root
 
 # Pull the Docker image from Docker Hub
-docker pull techwithalok/codepipeline-demo
+docker pull jagseersingh/codepipeline-demo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 techwithalok/codepipeline-demo
+docker run -d -p 5000:5000 jagseersingh/codepipeline-demo
--- stop_container.sh
+++ stop_container.sh
@@ -3,6 +3,6 @@ set -e
 
 #sudo su - root
 # Stop the running container (if any)
-#docker stop $(docker ps -a -q)
-#docker rm $(docker ps -a -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 echo "done"
