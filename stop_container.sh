7d729db34a25de1045a11a77febff6893988779b
js-talentelgia <122963266+js-talentelgia@users.noreply.github.com>
2025-08-22 10:35:58 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -e
+# Get all container IDs
+containers=$(docker ps -aq)
+
+if [ -n "$containers" ]; then
+  echo "Stopping containers..."
+  docker stop $containers
+
+  echo "Removing containers..."
+  docker rm $containers
+else
+  echo "No containers found to stop or remove."
+fi
 
 echo "done"
